4fe08f82807ef028ea3be476c7250b07c62b6567
Kaung Nyan Lin <darkliyznyanlin910@gmail.com>
2025-01-23 16:19:46 +0800

    Temporal server setup

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
+COMPOSE_FILE="docker-compose.yml"
+if [ "$2" = "temporal" ]; then
+  COMPOSE_FILE="docker-compose-temporal.yml"
+fi
+
 if [ "$1" = "up" ]; then
-  docker compose --env-file .env.temporal up -d
+  docker compose -f $COMPOSE_FILE --env-file .env.temporal up -d
 elif [ "$1" = "down" ]; then
-  docker compose --env-file .env.temporal down
+  docker compose -f $COMPOSE_FILE --env-file .env.temporal down
 else
-  echo "Usage: $0 [up|down]"
+  echo "Usage: $0 [up|down] [temporal]"
   exit 1
 fi
