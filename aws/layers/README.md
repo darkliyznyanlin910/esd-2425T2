dc0e95e3174030e3c3aa00d40b1f7a61941ac320
Kaung Nyan Lin <darkliyznyanlin910@gmail.com>
2025-02-04 12:15:42 +0800

    removed terraform and aws

--- aws/layers/README.md
+++
@@ -1,3 +0,0 @@
-# What is this folder for?
-
-This folder is for deploying lambda layers to be used in AWS lambda deployments
--- terraform/environment.tf
+++
@@ -1,3 +0,0 @@
-locals {
-  environment_variables = { for tuple in regexall("(.*)=\"(.*)\"", file("../service-env/.env")) : tuple[0] => sensitive(tuple[1]) }
-}
