05e133b20d181e7a9c9245c1163446bad35deec8
Kaung Nyan Lin <darkliyznyanlin910@gmail.com>
2025-02-04 14:07:35 +0800

    Added OpenAPI and Scalar UI

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,4 +1,6 @@
+import { apiReference } from "@scalar/hono-api-reference";
 import { Hono } from "hono";
+import { openAPISpecs } from "hono-openapi";
 
 import { testRouter } from "./routers/test";
 import { test2Router } from "./routers/test2";
@@ -9,5 +11,26 @@ const routes = app
   .get("/", (c) => c.json({ ok: true }))
   .route("/test", testRouter)
   .route("/test2", test2Router);
+
+app.get(
+  "/openapi",
+  openAPISpecs(app, {
+    documentation: {
+      info: {
+        title: "Hono API",
+        version: "1.0.0",
+        description: "Greeting API",
+      },
+    },
+  }),
+);
+
+app.get(
+  "/docs",
+  apiReference({
+    theme: "saturn",
+    spec: { url: "/openapi" },
+  }),
+);
 
 export { app, routes };
--- apps/api/src/routers/test.ts
+++ apps/api/src/routers/test.ts
@@ -1,9 +1,26 @@
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
+import { describeRoute } from "hono-openapi";
+import { resolver } from "hono-openapi/zod";
 import { z } from "zod";
+
+const responseSchema = z.object({
+  message: z.string(),
+});
 
 const testRouter = new Hono().get(
   "/hello",
+  describeRoute({
+    description: "Say hello to the user",
+    responses: {
+      200: {
+        description: "Successful response",
+        content: {
+          "text/plain": { schema: resolver(responseSchema) },
+        },
+      },
+    },
+  }),
   zValidator(
     "query",
     z.object({
