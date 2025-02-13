a638ec07309c6a0ad4a2b0911c3a6027c936a499
RicePuddn <153080490+RicePuddn@users.noreply.github.com>
2025-02-13 13:39:20 +0800

    Migrated API

--- apps/api/src/routers/test.ts
+++ apps/api/src/routers/test.ts
@@ -1,37 +1,28 @@
-import { zValidator } from "@hono/zod-validator";
-import { Hono } from "hono";
-import { describeRoute } from "hono-openapi";
-import { resolver } from "hono-openapi/zod";
+import { createRoute, OpenAPIHono } from "@hono/zod-openapi";
 import { z } from "zod";
 
-const responseSchema = z.object({
-  message: z.string(),
-});
-
-const testRouter = new Hono().get(
-  "/hello",
-  describeRoute({
-    description: "Say hello to the user",
+const testRouter = new OpenAPIHono().openapi(
+  createRoute({
+    method: "get",
+    path: "/",
     responses: {
       200: {
-        description: "Successful response",
         content: {
-          "text/plain": { schema: resolver(responseSchema) },
+          "application/json": {
+            schema: z.object({
+              message: z.string(),
+            }),
+          },
         },
+        description: "Test response",
+      },
+      401: {
+        description: "Unauthorized",
       },
     },
   }),
-  zValidator(
-    "query",
-    z.object({
-      name: z.string(),
-    }),
-  ),
-  (c) => {
-    const { name } = c.req.valid("query");
-    return c.json({
-      message: `Hello! ${name} from test`,
-    });
+  async (c) => {
+    return c.json({ message: "Hello, world!" });
   },
 );
 
