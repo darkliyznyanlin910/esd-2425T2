f97ffb77a008ba72426cbea0dab385de79c94328
Kaung Nyan Lin <darkliyznyanlin910@gmail.com>
2025-02-17 00:39:30 +0800

    Added driver and order databases

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,13 +1,12 @@
 #! /bin/bash
 COMPOSE_FILE="docker-compose.yml"
-ENV_FILE=".env.temporal"
 if [ "$2" = "temporal" ]; then
   COMPOSE_FILE="docker-compose-temporal.yml"
+  ENV_FILE=".env.temporal"
 elif [ "$2" = "databases" ]; then
   COMPOSE_FILE="docker-compose-databases.yml"
 elif [ "$2" = "localstack" ]; then
   COMPOSE_FILE="docker-compose-localstack.yml"
-  ENV_FILE=""
 else
   echo "Usage: $0 (up|down|clean) [temporal|databases|localstack]"
   exit 1
@@ -18,6 +17,9 @@ if [ "$1" = "up" ]; then
     docker compose -f $COMPOSE_FILE up -d
   else
     docker compose -f $COMPOSE_FILE --env-file $ENV_FILE -d
+  fi
+  if [ "$2" = "databases" ]; then
+    pnpm run db:push
   fi
 elif [ "$1" = "down" ]; then
   if [ -z "$ENV_FILE" ]; then
