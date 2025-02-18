4d6b59ee5fb868024bda955b0cd77a8a5da91bfc
Kaung Nyan Lin <darkliyznyanlin910@gmail.com>
2025-02-18 12:12:17 +0800

    Changed to use temporal logger

--- temporal/activities/src/index.ts
+++ temporal/activities/src/index.ts
@@ -1,34 +1,43 @@
+import { log } from "@temporalio/activity";
+
 import { Invoice, Order, OrderStatus } from "@repo/temporal-common";
 
 export async function updateOrderStatus(
   orderId: Order["id"],
   status: OrderStatus,
-): Promise<void> {}
+): Promise<void> {
+  log.info("Updating order status", { orderId, status });
+}
 
 export async function getOrderStatus(
   orderId: Order["id"],
 ): Promise<OrderStatus> {
+  log.info("Getting order status", { orderId });
   return "processing";
 }
 
 export async function sendOrderToDrivers(order: Order): Promise<void> {
-  console.log("Sending order to drivers", JSON.stringify(order, null, 2));
+  log.info("Sending order to drivers", {
+    order: JSON.stringify(order, null, 2),
+  });
 }
 
 export async function notifyAdmin(order: Order): Promise<void> {
-  console.log("Notifying admin", JSON.stringify(order, null, 2));
+  log.info("Notifying admin", { order: JSON.stringify(order, null, 2) });
 }
 
 export async function assignOrderToDriver(
   order: Order,
   driverId: string,
 ): Promise<void> {
-  console.log("Assigning order to driver", JSON.stringify(order, null, 2));
-  console.log("Driver ID", driverId);
+  log.info("Assigning order to driver", {
+    order: JSON.stringify(order, null, 2),
+    driverId,
+  });
 }
 
 export async function generateInvoice(order: Order): Promise<Invoice> {
-  return {
+  const temp: Invoice = {
     id: "1",
     orderId: order.id,
     customerId: order.userId,
@@ -38,8 +47,12 @@ export async function generateInvoice(order: Order): Promise<Invoice> {
     createdAt: new Date(),
     updatedAt: new Date(),
   };
+  log.info("Generated invoice", { invoice: JSON.stringify(temp, null, 2) });
+  return temp;
 }
 
 export async function sendInvoiceToCustomer(invoice: Invoice): Promise<void> {
-  console.log("Sending invoice to customer", JSON.stringify(invoice, null, 2));
+  log.info("Sending invoice to customer", {
+    invoice: JSON.stringify(invoice, null, 2),
+  });
 }
