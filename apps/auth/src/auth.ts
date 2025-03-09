18f7de290a4745f23f2cb55502e97004eed95af8
RicePuddn <153080490+RicePuddn@users.noreply.github.com>
2025-03-09 17:10:26 +0800

    Attempted auth changes

--- apps/auth/src/auth.ts
+++ apps/auth/src/auth.ts
@@ -28,7 +28,17 @@ export const auth = betterAuth({
     openAPI({
       path: "/docs",
     }),
-    admin(),
+    admin({
+      adminRole: ["admin", "driver"],
+      defaultRole: "client",
+      schema: {
+        user: {
+          fields: {
+            banned: "disabled",
+          },
+        },
+      },
+    }),
   ],
   trustedOrigins: SERVICES.map((service) => getServiceBaseUrl(service)),
 });
