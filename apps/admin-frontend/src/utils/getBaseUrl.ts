91397ce7b18a2007924e17f8749caf75b7c448c8
Kaung Nyan Lin <darkliyznyanlin910@gmail.com>
2025-03-11 23:26:47 +0800

    Fixed ENV

--- apps/admin-frontend/src/utils/getBaseUrl.ts
+++
@@ -1,9 +0,0 @@
-import { getServiceBaseUrl } from "@repo/service-discovery";
-
-export const getBaseUrl = () => {
-  return getServiceBaseUrl("web");
-};
-
-export const getApiBaseUrl = () => {
-  return getServiceBaseUrl("api");
-};
