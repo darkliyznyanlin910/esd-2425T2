8e4dddfdbafe7ce1dbd9127c2b1e22fcb54ee77e
Felicia Tanujaya <148226847+FeliciaaaTan@users.noreply.github.com>
2025-03-14 23:19:09 +0800

    init admin-frontend (order)

---
+++ apps/admin-frontend/src/app/dashboard/components/driver/columns.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown } from "lucide-react";
+
+import { Order } from "@repo/db-order/zod";
+import { Button } from "@repo/ui/button";
+
+export const columns: ColumnDef<Order>[] = [
+  {
+    accessorKey: "id",
+    header: "ID",
+  },
+  {
+    accessorKey: "userId",
+    header: "Customer",
+  },
+  {
+    header: "From Address",
+    cell: ({ row }) => {
+      const { fromAddressLine1, fromAddressLine2, fromZipCode } = row.original;
+      return `${fromAddressLine1} ${fromAddressLine2} ${fromZipCode}`;
+    },
+  },
+  {
+    header: "To Address",
+    cell: ({ row }) => {
+      const { toAddressLine1, toAddressLine2, toZipCode } = row.original;
+      return `${toAddressLine1} ${toAddressLine2} ${toZipCode}`;
+    },
+  },
+  {
+    accessorKey: "orderStatus",
+    header: "Status",
+  },
+  {
+    accessorKey: "createdAt",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          className="p-0"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Created At
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "updatedAt",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          className="p-0"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Updated At
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+];
