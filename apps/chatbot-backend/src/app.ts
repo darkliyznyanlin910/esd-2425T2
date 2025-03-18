d890722afd771ea1b7180e051cf7818573e23190
Kaung Nyan Lin <darkliyznyanlin910@gmail.com>
2025-03-18 18:54:28 +0800

    Lint fixed

--- apps/chatbot-backend/src/app.ts
+++ apps/chatbot-backend/src/app.ts
@@ -38,13 +38,13 @@ const routes = app
       spec: { url: `${getServiceBaseUrl("chatbot")}/openapi` },
     }),
   )
-  // .use(
-  //   authMiddleware({
-  //     authBased: {
-  //       allowedRoles: ["client"],
-  //     },
-  //   }),
-  // )
+  .use(
+    authMiddleware({
+      authBased: {
+        allowedRoles: ["client"],
+      },
+    }),
+  )
   .route("/chat", chatRouter);
 
 export { app, routes };
