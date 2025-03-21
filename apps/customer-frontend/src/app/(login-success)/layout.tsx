b1b2dbd2b3ae445e582058befc30e1c4e9cf01fa
RicePuddn <153080490+RicePuddn@users.noreply.github.com>
2025-03-21 13:29:31 +0800

    separated sidebar for customer ui

---
+++ apps/customer-frontend/src/app/(login-success)/layout.tsx
@@ -0,0 +1,15 @@
+import { SidebarProvider, SidebarTrigger } from "@repo/ui/sidebar";
+
+import { AppSidebar } from "../components/app-sidebar";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main className="flex h-screen w-full flex-1 flex-col gap-4 bg-slate-50 p-4 pt-2">
+        <div className="flex items-center gap-3"></div>
+        {children}
+      </main>
+    </SidebarProvider>
+  );
+}
