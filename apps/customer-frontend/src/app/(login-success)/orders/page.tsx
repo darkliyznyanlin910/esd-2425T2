6cfa0cdefd737e664b575b3bbf67d9b0c88b1096
itayush <87350108+Yati2@users.noreply.github.com>
2025-03-27 14:23:03 +0800

    customer-frontend structure cleaned up

---
+++ apps/customer-frontend/src/app/(login-success)/orders/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+import { authClient } from "@repo/auth/client";
+
+import OrderTable from "~/app/components/orders";
+
+export default function main() {
+  const { useSession } = authClient;
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!session) {
+      router.push("/auth");
+    }
+  }, [session, router]);
+  return <OrderTable />;
+}
