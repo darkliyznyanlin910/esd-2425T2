6cd1d702d1538297aa2e4657d9246f8fe805bd72
Felicia Tanujaya <148226847+FeliciaaaTan@users.noreply.github.com>
2025-03-27 14:59:53 +0800

    pdfffff

--- temporal/workflows/src/b2cOrder.ts
+++ temporal/workflows/src/b2cOrder.ts
@@ -161,7 +161,7 @@ Created at: ${order.createdAt.toLocaleString()}`,
     const invoice = await generateInvoice(
       order,
       stripeInvoice.amount_due / 100,
-      stripeInvoice.hosted_invoice_url ?? undefined,
+      stripeInvoice.invoice_pdf ?? undefined,
       stripeInvoice.id,
     );
     await sendInvoiceToCustomer(invoice);
