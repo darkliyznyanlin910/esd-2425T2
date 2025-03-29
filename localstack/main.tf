b1afb86dfc65a0e599548dbacc3c9f5d20f8efc3
Felicia Tanujaya <148226847+FeliciaaaTan@users.noreply.github.com>
2025-03-29 20:16:42 +0800

    test

--- localstack/main.tf
+++ localstack/main.tf
@@ -10,7 +10,7 @@ terraform {
 provider "aws" {
   access_key                  = "test"
   secret_key                  = "test"
-  region                      = "us-east-1"
+  region                      = "ap-southeast-1"
   skip_credentials_validation = true
   skip_requesting_account_id  = true
   skip_metadata_api_check     = true
