7b37f553dd720765ce2a7437293c7e58ce3a245b
Kaung Nyan Lin <darkliyznyanlin910@gmail.com>
2025-04-06 20:00:16 +0800

    Added read replica

--- packages/db-invoice/env.ts
+++ packages/db-invoice/env.ts
@@ -4,12 +4,18 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     INVOICE_POSTGRES_URL: z.string(),
+    INVOICE_POSTGRES_READ_REPLICA_URLS: z
+      .string()
+      .transform((val) => val.split(","))
+      .default(""),
     NODE_ENV: z
       .enum(["development", "production", "staging"])
       .default("development"),
   },
   runtimeEnv: {
     INVOICE_POSTGRES_URL: process.env.INVOICE_POSTGRES_URL,
+    INVOICE_POSTGRES_READ_REPLICA_URLS:
+      process.env.INVOICE_POSTGRES_READ_REPLICA_URLS,
     NODE_ENV: process.env.NODE_ENV,
   },
   skipValidation: process.env.npm_lifecycle_event === "lint",
--- packages/db-order/src/index.ts
+++ packages/db-order/src/index.ts
@@ -1,10 +1,16 @@
+import { readReplicas } from "@prisma/extension-read-replicas";
+
 import { env } from "../env";
 import { PrismaClient } from "./generated/client";
 
 const createPrismaClient = () =>
   new PrismaClient({
     log: env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
-  });
+  }).$extends(
+    readReplicas({
+      url: env.ORDER_POSTGRES_READ_REPLICA_URLS,
+    }),
+  );
 
 const globalForPrisma = globalThis as unknown as {
   orderDb: ReturnType<typeof createPrismaClient> | undefined;
