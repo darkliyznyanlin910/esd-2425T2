58a07cbf6d42638229dcd2a5c7e9cc279f438f59
RicePuddn <153080490+RicePuddn@users.noreply.github.com>
2025-04-08 23:41:41 +0800

    fuck it

--- apps/notification/src/type.ts
+++ apps/notification/src/type.ts
@@ -10,6 +10,7 @@ export const HonoClient = hc<typeof routes>(getServiceBaseUrl("notification"));
 export interface DriverEventHandlers {
   broadcastOrder: (order: Order) => void;
   invalidateOrder: (order: Order) => void;
+  manualAssignment: (data: { order: Order; driverId?: string }) => void;
 }
 export interface AdminEventHandlers {
   receiveDelay: (order: Order) => void;
